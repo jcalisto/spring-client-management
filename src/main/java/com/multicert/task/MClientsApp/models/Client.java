f070d743601d0d426270d0716358795bf4c09797
jcalisto <jpsc_bk@hotmail.com>
2020-09-27 17:40:36 +0100

    change client id strategy

--- src/main/java/com/multicert/task/MClientsApp/models/Client.java
+++ src/main/java/com/multicert/task/MClientsApp/models/Client.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 public class Client {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
